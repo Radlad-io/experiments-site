d33c83529e681f60db6d81e52d8ddb2b53959215
Kevin Merinsky <kevinmerinsky@gmail.com>
2021-12-28 23:09:44 -0500

    Updated homepage layout

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -97,6 +97,23 @@ export default function Home() {
         <Container>
           <main className={styles.wrapper}>
             <motion.h1 variants={heading}>Experiments</motion.h1>
+            <motion.ul variants={heading} className={styles.legend}>
+              <p>
+                <b>Legend: </b>
+              </p>
+              <li className={styles.wip}>
+                <p>Work in progress</p>
+              </li>
+              <li className={styles.tutorial}>
+                <p>Tutorial Based</p>
+              </li>
+              <li className={styles.demo}>
+                <p>Demo</p>
+              </li>
+              <li className={styles.project}>
+                <p>Project</p>
+              </li>
+            </motion.ul>
             <div className={styles.grid}>
               <motion.article
                 variants={articles}
@@ -126,14 +143,6 @@ export default function Home() {
                       Getting through the book of shaders
                     </Link>
                   </li>
-                  <li>
-                    <Link
-                      href="https://www.awwwards.com/academy/course/audio-reactive-visuals-with-code"
-                      passHref
-                    >
-                      Audio visualizations with P5.js
-                    </Link>
-                  </li>
                 </ul>
               </motion.article>
               <motion.article
@@ -143,56 +152,56 @@ export default function Home() {
               >
                 <h2>Framer Motion</h2>
                 <ul>
-                  <li>
+                  <li className={styles.demo}>
                     <Link href="/framer-motion/001" passHref scroll={false}>
                       Orchestration + State
                     </Link>
                   </li>
-                  <li>
+                  <li className={styles.demo}>
                     <Link href="/framer-motion/001" passHref scroll={false}>
                       Adding Masks
                     </Link>
                   </li>
-                  <li>
+                  <li className={styles.demo}>
                     <Link href="/framer-motion/001" passHref scroll={false}>
                       Keyframes
                     </Link>
                   </li>
-                  <li>
+                  <li className={styles.demo}>
                     <Link href="/framer-motion/002" passHref scroll={false}>
                       Intersection Observer
                     </Link>
                   </li>
-                  <li>
+                  <li className={styles.demo}>
                     <Link href="/framer-motion/003" passHref scroll={false}>
                       Parallax effect
                     </Link>
                   </li>
-                  <li>
+                  <li className={styles.demo}>
                     <Link href="/framer-motion/003" passHref scroll={false}>
                       Custom cursor
                     </Link>
                   </li>
-                  <li>
+                  <li className={styles.demo}>
                     <Link href="/framer-motion/001" passHref scroll={false}>
                       Drag + Drop
                     </Link>
                   </li>
-                  <li>
+                  <li className={styles.demo}>
                     <Link href="/framer-motion/001" passHref scroll={false}>
                       Shared layout
                     </Link>
                   </li>
                 </ul>
-                <div className={styles.disclaimer}>
-                  <i className={`material-icons-outlined`}>construction</i>
-                  <p>Work in progress</p>
-                </div>
               </motion.article>
-              <motion.article variants={articles} className={styles.block}>
+              <motion.article
+                variants={articles}
+                className={styles.block}
+                id={styles.threejs}
+              >
                 <h2>Three.js</h2>
                 <ul>
-                  <li>
+                  <li className={styles.wip}>
                     <Link
                       href="https://lander-game.kevinmerinsky.com/"
                       passHref
@@ -201,7 +210,7 @@ export default function Home() {
                       Lander Game
                     </Link>
                   </li>
-                  <li>
+                  <li className={styles.tutorial}>
                     <Link
                       href="https://experiments-traffic-game.vercel.app/"
                       passHref
@@ -210,7 +219,7 @@ export default function Home() {
                       Traffic Game
                     </Link>
                   </li>
-                  <li>
+                  <li className={styles.tutorial}>
                     <Link
                       href="/threejs/particle-meshes_shaders"
                       passHref
@@ -221,58 +230,67 @@ export default function Home() {
                   </li>
                 </ul>
               </motion.article>
-              <motion.article
-                variants={articles}
-                className={styles.block}
-                id={styles.comingSoon}
-              >
+              <motion.article variants={articles} className={styles.block}>
                 <h2>AFrame</h2>
-                <p>Coming soon.</p>
+                <div id="vertFlex">
+                  <ul>
+                    <li>
+                      <Link href="https://ar.uarts.edu" passHref scroll={false}>
+                        Augmented Reality App
+                      </Link>
+                    </li>
+                  </ul>
+                  <div className={styles.disclaimer}>
+                    <i className={`material-icons-outlined`}>
+                      report_gmailerrorred
+                    </i>
+                    <p>Requires printed material</p>
+                  </div>
+                </div>
               </motion.article>
-              <motion.article
-                variants={articles}
-                className={styles.block}
-                id={styles.electron}
-              >
-                <h2>RadBooth</h2>
+              <motion.article variants={articles} className={styles.block}>
+                <h2>Arduino</h2>
                 <ul>
                   <li>
                     <Link
-                      href="https://github.com/Radlad-io/RadBooth-App"
+                      href="https://www.youtube.com/watch?v=G_-dKWhOJTg"
                       passHref
                       scroll={false}
                     >
                       <a>
-                        Application
-                        <i className={`material-icons-outlined`}>open_in_new</i>
-                      </a>
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      href="https://github.com/Radlad-io/radbooth-service"
-                      passHref
-                      scroll={false}
-                    >
-                      <a>
-                        Service
+                        Slo-Mo Video Booth
                         <i className={`material-icons-outlined`}>open_in_new</i>
                       </a>
                     </Link>
                   </li>
                 </ul>
-                <div className={styles.disclaimer}>
-                  <i className={`material-icons-outlined`}>construction</i>
-                  <p>Work in progress</p>
-                </div>
               </motion.article>
-              <motion.article
-                variants={articles}
-                className={styles.block}
-                id={styles.comingSoon}
-              >
+              <motion.article variants={articles} className={styles.block}>
                 <h2>Electron</h2>
-                <p>Coming soon.</p>
+                <div id="vertFlex">
+                  <ul>
+                    <li>
+                      <Link
+                        href="https://github.com/Radlad-io/RadBooth-App"
+                        passHref
+                        scroll={false}
+                      >
+                        <a>
+                          Application
+                          <i className={`material-icons-outlined`}>
+                            open_in_new
+                          </i>
+                        </a>
+                      </Link>
+                    </li>
+                  </ul>
+                  <div className={styles.disclaimer}>
+                    <i className={`material-icons-outlined`}>
+                      report_gmailerrorred
+                    </i>
+                    <p>Requires special hardware</p>
+                  </div>
+                </div>
               </motion.article>
               <motion.article
                 variants={articles}
@@ -290,14 +308,93 @@ export default function Home() {
                 <h2>Node.JS</h2>
                 <ul>
                   <li>
-                    <Link href="https://github.com/Radlad-io/Soundbot" passHref>
-                      <div>
+                    <Link
+                      href="https://github.com/Radlad-io/Soundbot"
+                      passHref
+                      scroll={false}
+                    >
+                      <a>
                         SoundBot
                         <i className={`material-icons-outlined`}>open_in_new</i>
-                      </div>
+                      </a>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      href="https://github.com/Radlad-io/radbooth-service"
+                      passHref
+                      scroll={false}
+                    >
+                      <a>
+                        Service
+                        <i className={`material-icons-outlined`}>open_in_new</i>
+                      </a>
                     </Link>
                   </li>
                 </ul>
+              </motion.article>
+              <motion.article
+                variants={articles}
+                className={styles.block}
+                id={styles.webXr}
+              >
+                <h2>WebXR API</h2>
+                <div id={styles.vertFlex}>
+                  <ul>
+                    <li className={styles.tutorial}>
+                      <Link href="/webxr/001" passHref scroll={false}>
+                        Basic Scene
+                      </Link>
+                    </li>
+                    <li className={styles.tutorial}>
+                      <Link href="/webxr/002" passHref scroll={false}>
+                        Basic Scene + WebXR
+                      </Link>
+                    </li>
+                    <li className={styles.tutorial}>
+                      <Link href="/webxr/003" passHref scroll={false}>
+                        Additional Objects
+                      </Link>
+                    </li>
+                    <li className={styles.tutorial}>
+                      <Link href="/webxr/004" passHref scroll={false}>
+                        Additional Objects + Animation
+                      </Link>
+                    </li>
+                    <li className={styles.tutorial}>
+                      <Link href="/webxr/005" passHref scroll={false}>
+                        Model loading
+                      </Link>
+                    </li>
+                    <li className={styles.tutorial}>
+                      <Link href="/webxr/006" passHref scroll={false}>
+                        Animated Model
+                      </Link>
+                    </li>
+                    <li className={styles.tutorial}>
+                      <Link href="/webxr/007" passHref scroll={false}>
+                        Touch gestures
+                      </Link>
+                    </li>
+                    <li className={styles.tutorial}>
+                      <Link href="/webxr/008" passHref scroll={false}>
+                        Hit testing (reticle only)
+                      </Link>
+                    </li>
+                    <li className={styles.tutorial}>
+                      <Link href="/webxr/009" passHref scroll={false}>
+                        Hit testing
+                      </Link>
+                    </li>
+                  </ul>
+
+                  <div className={styles.disclaimer}>
+                    <i className={`material-icons-outlined`}>
+                      report_gmailerrorred
+                    </i>
+                    <p>Requires special browser</p>
+                  </div>
+                </div>
               </motion.article>
               <motion.article
                 variants={articles}
@@ -309,9 +406,8 @@ export default function Home() {
               </motion.article>
               <motion.article variants={articles} className={styles.block}>
                 <h2>P5.JS</h2>
-
                 <ul>
-                  <li>
+                  <li className={styles.tutorial}>
                     <Link
                       href="/p5/01_simple-canvas-drawing"
                       passHref
@@ -320,7 +416,7 @@ export default function Home() {
                       Simple Canvas
                     </Link>
                   </li>
-                  <li>
+                  <li className={styles.tutorial}>
                     <Link
                       href="/p5/02_input-interactions"
                       passHref
@@ -329,12 +425,12 @@ export default function Home() {
                       User Interactions
                     </Link>
                   </li>
-                  <li>
+                  <li className={styles.tutorial}>
                     <Link href="/p5/03_audio_input" passHref scroll={false}>
                       Audio Input
                     </Link>
                   </li>
-                  <li>
+                  <li className={styles.tutorial}>
                     <Link
                       href="/p5/04_static_audio_waveform"
                       passHref
@@ -343,7 +439,7 @@ export default function Home() {
                       Audio Waveform
                     </Link>
                   </li>
-                  <li>
+                  <li className={styles.tutorial}>
                     <Link
                       href="/p5/05_amplitude_and_waveform"
                       passHref
@@ -357,75 +453,16 @@ export default function Home() {
               <motion.article
                 variants={articles}
                 className={styles.block}
-                id={styles.webXr}
-              >
-                <h2>WebXR API</h2>
-                <ul>
-                  <li>
-                    <Link href="/webxr/001" passHref scroll={false}>
-                      Basic Scene
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="/webxr/002" passHref scroll={false}>
-                      Basic Scene + WebXR
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="/webxr/003" passHref scroll={false}>
-                      Additional Objects
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="/webxr/004" passHref scroll={false}>
-                      Additional Objects + Animation
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="/webxr/005" passHref scroll={false}>
-                      Model loading
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="/webxr/006" passHref scroll={false}>
-                      Animated Model
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="/webxr/007" passHref scroll={false}>
-                      Touch gestures
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="/webxr/008" passHref scroll={false}>
-                      Hit testing (reticle only)
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="/webxr/009" passHref scroll={false}>
-                      Hit testing
-                    </Link>
-                  </li>
-                </ul>
-
-                <div className={styles.disclaimer}>
-                  <i className={`material-icons-outlined`}>warning</i>
-                  <p>Requires special browser</p>
-                </div>
-              </motion.article>
-              <motion.article
-                variants={articles}
-                className={styles.block}
                 id={styles.nodejs}
               >
                 <h2>Blender</h2>
                 <ul>
-                  <li>
+                  <li className={styles.tutorial}>
                     <Link href="/blender/001" passHref scroll={false}>
                       Physics animation
                     </Link>
                   </li>
-                  <li>
+                  <li className={styles.tutorial}>
                     <Link
                       href="/blender/models/forest-road"
                       passHref
@@ -435,14 +472,6 @@ export default function Home() {
                     </Link>
                   </li>
                 </ul>
-              </motion.article>
-              <motion.article
-                variants={articles}
-                className={styles.block}
-                id={styles.comingSoon}
-              >
-                <h2>After Effects</h2>
-                <p>Coming soon.</p>
               </motion.article>
               <motion.article
                 variants={articles}

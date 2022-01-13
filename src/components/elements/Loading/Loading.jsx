6f19b143f78bf0c7a8a53f883fc1c4ac356534fc
Kevin Merinsky <kevinmerinsky@gmail.com>
2022-01-13 13:42:21 -0500

    Loading component

---
+++ src/components/elements/Loading/Loading.jsx
@@ -0,0 +1,8 @@
+import styles from "./Loading.module.scss";
+import * as variants from "./Loading.motion";
+
+function Loading() {
+  return <div className="classic-5"></div>;
+}
+
+export default Loading;

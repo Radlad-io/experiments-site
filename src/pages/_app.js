14685f4497bb730f1fd0df34144b503a3c0f690d
Kevin Merinsky <kevinmerinsky@gmail.com>
2022-01-17 18:44:02 -0500

    Fix: Scroll to top after exit animation

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -29,7 +29,10 @@ function MyApp({ Component, pageProps, router }) {
   }, []);
   return (
     <>
-      <AnimatePresence exitBeforeEnter>
+      <AnimatePresence
+        exitBeforeEnter
+        onExitComplete={() => window.scrollTo(0, 0)}
+      >
         <Component {...pageProps} key={router.route} />
       </AnimatePresence>
     </>

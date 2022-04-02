4dd55502baf3730c55b9df44c28c3cfcc45ae620
Kevin Merinsky <kevinmerinsky@gmail.com>
2022-04-02 18:57:44 -0400

    Updated Video.jsx to new S3 bucket

--- src/components/elements/Video/Video.jsx
+++ src/components/elements/Video/Video.jsx
@@ -1,14 +1,16 @@
 import styles from "./Video.module.scss";
-import {motion} from 'framer-motion';
-;
-
-
+import { motion } from "framer-motion";
 export default function Video() {
   return (
     <>
-      <motion.div className={styles.wrapper} initial={{opacity: 0}} animate={{opacity: 1, transition: {duration: .75}}} exit={{opacity: 0, transition: {duration: 0.75} }}>
+      <motion.div
+        className={styles.wrapper}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1, transition: { duration: 0.75 } }}
+        exit={{ opacity: 0, transition: { duration: 0.75 } }}
+      >
         <video
-          src="https://s3.blk.studio/Personal/Experiments/Blender/001/Blender_Physics%20001.mp4"
+          src="https://s3.fervor.nyc3.digitaloceanspaces.com/personal/experiments/blender/Blender_Physics.mp4"
           autoPlay
           playsInline
           muted
